82ee7a7b9b8735a22ae1d1df769974bcc8e0e5f8
Oussama Nahiz <useit015@gmail.com>
2023-10-02 17:19:47 +0100

    Updated redux store

--- src/store/sliceChartFilters.js
+++ src/store/sliceChartFilters.js
@@ -1,22 +1,67 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const getFilterInitialValues = createAsyncThunk(
-  'chartFilters/getValues',
-  async (_, { rejectWithValue }) =>
-    fetch('http://demo8449112.mockable.io/filters')
-      .then(res =>
-        res.ok ? res.json() : rejectWithValue([], 'error while fetching data')
-      )
-      .catch(error => rejectWithValue([], error))
-);
+const extractValues = (data = [], key, label = key) =>
+  data.map(item => ({
+    id: item.id,
+    value: item[key],
+    label: item[label] === 'Sample' ? 'All' : item[label]
+  }));
+
+const reducePayload = (payload, accessKey, valueKey, labelKey = valueKey) =>
+  payload.reduce((acc, item) => {
+    const { id, [valueKey]: value, [accessKey]: key, [labelKey]: label } = item;
+
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+
+    acc[key].push({
+      id,
+      value,
+      label
+    });
+
+    return acc;
+  }, {});
+
+const getFilterValues = payload => ({
+  demo: extractValues(payload.demo, 'demo', 'demo_fantasy'),
+  topic: extractValues(payload.topic, 'topic'),
+  location: extractValues(payload.location, 'location'),
+  timeframe: extractValues(payload.timeframe, 'timeframe'),
+  action: reducePayload(payload.action, 'topic', 'action'),
+  demo_cut: reducePayload(payload.demo_cut, 'demo', 'demo_cut')
+});
+
+const getDefaultFilters = ({ filters, values }) => ({
+  ...filters,
+  demo: values.demo[1],
+  topic: values.topic[0],
+  action: values.action[values.topic[0].value][0]
+});
+
+const getSingleChartData = payload =>
+  payload.map(
+    ({
+      demo_cut_fantasy,
+      integer_value,
+      float_value,
+      value_type,
+      demo_cut
+    }) => ({
+      label: demo_cut_fantasy === 'Sample' ? 'All' : demo_cut_fantasy,
+      value: value_type ? float_value : integer_value,
+      demo_cut
+    })
+  );
 
 const filters = {
-  demographic: '',
+  demo: '',
   location: '',
-  demoCut: '',
+  demo_cut: '',
   topic: '',
   action: '',
-  time: ''
+  timeframe: ''
 };
 
 const sliceChartFilters = createSlice({
@@ -24,48 +69,54 @@ const sliceChartFilters = createSlice({
 
   initialState: {
     filters,
+    data: [],
     values: {},
+    mode: 'topic',
     status: 'loading'
   },
 
   reducers: {
-    resetFilters: ({ values, status }) => ({
-      status,
-      values,
-      filters
-    }),
-
     setFilter: (state, { payload }) => {
       const { name, value } = payload;
 
       state.filters[name] = value;
 
+      // Set a default value for the action filter when `topic` has changed
       if (
         name === 'topic' &&
+        state.mode === 'topic' &&
         !state.values.action[value]?.includes(state.filters.action)
       ) {
-        state.filters.action = '';
+        state.filters.action =
+          state.values.action[state.filters.topic.value][0];
       }
-    }
-  },
-
-  extraReducers: {
-    [getFilterInitialValues.pending]: state => {
-      state.status = 'loading';
     },
 
-    [getFilterInitialValues.fulfilled]: (state, { payload }) => {
-      state.values = payload;
-
+    initFilters: (state, { payload }) => {
       state.status = 'success';
+
+      state.values = getFilterValues(payload);
+
+      state.filters = getDefaultFilters(state);
+
+      console.log('payload :>> ', { ...state.values });
     },
 
-    [getFilterInitialValues.rejected]: state => {
-      state.status = 'failed';
+    toggleMode: state => {
+      if (state.mode === 'topic') {
+        state.mode = 'demo';
+      } else {
+        state.mode = 'topic';
+      }
+    },
+
+    setSingleChartData: (state, { payload }) => {
+      state.data = getSingleChartData(payload);
     }
   }
 });
 
-export const { setFilter, resetFilters } = sliceChartFilters.actions;
+export const { setFilter, resetFilters, toggleMode } =
+  sliceChartFilters.actions;
 
 export default sliceChartFilters.reducer;
